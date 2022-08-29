3eedbdb8abd43feb72a21047821ec98c516c418c
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-28 21:04:20 -0500

    refactor

---
+++ src/main/java/com/bank/msyankitransactionbatch/MsYankiTransactionBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msyankitransactionbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsYankiTransactionBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsYankiTransactionBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/msyankitransactionbatch/MsYankiTransactionBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msyankitransactionbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsYankiTransactionBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
